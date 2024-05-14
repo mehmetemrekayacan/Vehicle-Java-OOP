e3e29dbf0a78f5a37074b84c09dc378f25508db6
emremehmet32@hotmail.com <emremehmet32@hotmail.com>
2024-05-14 22:54:37 +0300

    add mainfuntion class

---
+++ src/Class/MainFunction.java
@@ -0,0 +1,59 @@
+package Class;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MainFunction {
+    private int function;
+    private int functionCounter = 0;
+    private int yesNo;
+    public Random random = new Random();
+    public Scanner input = new Scanner(System.in);
+
+    public void Go(Vehicle name, int pace) throws InterruptedException {
+        System.out.println("Star engine for enter 1\nLeave vehicle for 2:");
+        function = input.nextInt();
+        if (function == 2) {
+            System.out.println("Leaving vehicle...");
+            Thread.sleep(1000);
+            System.out.println("Left vehicle");
+            System.exit(0);
+        }
+        name.start();
+        while (function != 3) {
+            System.out.println("Enter the function: \n1. Accelerate\n2. Decelerate:");
+            function = input.nextInt();
+            Thread.sleep(500);
+            if (function == 1) {
+                name.accelerate(pace);
+                System.out.println("Speed: " + name.getSpeed() + " km/h");
+                System.out.println("Gasoline: " + name.getGasoline() + " liters");
+
+            } else if (function == 2) {
+                name.decelerate(pace);
+                System.out.println("Speed: " + name.getSpeed() + " km/h");
+                System.out.println("Gasoline: " + name.getGasoline() + " liters");
+
+            } else {
+                System.out.println("Invalid Input");
+            }
+            functionCounter++;
+            if (functionCounter % 10 == 0) {
+                System.out.println("You've been in the car for too long. Do you want to exit? (1: Yes, 2: No)");
+                yesNo = input.nextInt();
+                if (yesNo == 1) {
+                    System.out.println("Stopping vehicle...");
+                    Thread.sleep(1000);
+                    name.setSpeed(0);
+                    System.out.println("Speed: " + name.getSpeed() + " km/h");
+                    System.out.println("Exiting...");
+                    Thread.sleep(1000);
+                    System.out.println("Exited");
+                    System.exit(0);
+                } else {
+                    System.out.println("Continuing...");
+                    Thread.sleep(1000);
+                }
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-import Class.Vehicle;
+import Class.MainFunction;
 import Vehicles.Tank;
 import Vehicles.Truck;
 import Vehicles.Plane;
@@ -15,38 +15,36 @@ public class Main {
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
+        MainFunction mainFunction = new MainFunction();
         System.out.println("What are you going to do?\n1. Land Vehicle\n2. Air Vehicle\n3. Water Vehicle");
         typeVehicle = input.nextInt();
         if (typeVehicle == 1) {
-            Vehicle landVehicle = new LandVehicle();
             System.out.println("Enter the number of vehicles: \n1. Truck\n2. Tank");
             vehicles = input.nextInt();
             if (vehicles == 1) {
                 LandVehicle truck = new Truck();
-                landVehicle.Go(truck,truck.getVelocity());
+                mainFunction.Go(truck,truck.getVelocity());
             } else if (vehicles == 2) {
                 LandVehicle tank = new Tank();
-                landVehicle.Go(tank,tank.getVelocity());
+                mainFunction.Go(tank,tank.getVelocity());
             } else {
                 System.out.println("Invalid Input");
             }
         } else if (typeVehicle == 2) {
-            Vehicle airVehicle = new AirVehicle();
             System.out.println("Enter the number of vehicles: \n1. Truck\n2. Tank");
             vehicles = input.nextInt();
             if (vehicles == 1) {
                 AirVehicle plane = new Plane();
-                airVehicle.Go(plane,plane.getVelocity());
+                mainFunction.Go(plane,plane.getVelocity());
             }else {
                 System.out.println("Invalid Input");
             }
         } else if (typeVehicle == 3) {
-            Vehicle waterVehicle = new WaterVehicle();
             System.out.println("Enter the number of vehicles: \n1. Truck\n2. Tank");
             vehicles = input.nextInt();
             if (vehicles == 1) {
                 WaterVehicle boat = new Boat();
-                waterVehicle.Go(boat,boat.getVelocity());
+                mainFunction.Go(boat,boat.getVelocity());
             } else {
                 System.out.println("Invalid Input");
             }
